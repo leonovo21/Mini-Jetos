25f54e42085d0a27c567d5efd47f0d2043b94e88
leonovo21 <leonardo94615@gmail.com>
2023-12-20 22:20:35 -0300

    AppContent

--- html/crud/client/src/AppContent.js
+++ html/crud/client/src/AppContent.js
@@ -2,12 +2,42 @@ import  React, {Component} from 'react'
 
 
 export default class AppContent extends Component{
+
+    state = {posts: []}
+    constructor(props){
+        super(props)
+        this.listRef = React.createRef()
+    }
+    fetchList = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then(json => {
+                this.setState({posts: json})
+            })
+    }
+
+    clickdItem = (x) =>{
+        console.log("clicked", x)
+    }
+
     render(){
         return(
             <p>
-                This is the content
                 <br/>
-                <button class ="btn btn-primary" href='#'>Send</button>
+                <hr />
+                <button onClick={this.fetchList} class ="btn btn-primary" href='#'>Fetch Data</button>
+                <hr />
+
+                <ul>
+                    {this.state.posts.map((c) => (
+                        <li key={c.id}>
+                            <a href='#!' onClick={() => this.clickdItem(c.id)}>
+                                {c.title}
+                            </a>
+                        </li>
+                        
+                    ))}
+                </ul>
             </p>
 
         )
